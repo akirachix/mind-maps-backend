d29af0326d9948f8ec48ed80e7e610e40f338746
Immaculate <chepkiruiimmaculatekogo@gmail.com>
2025-06-20 16:01:43 +0300

    created farmer, admin, extension-worker models

---
+++ coopconnect/farmer/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Farmer(models.Model):
+    full_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    village_id = models.ForeignKey(Village, on_delete=models.SET_NULL, null=True)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
