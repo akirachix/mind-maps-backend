10ee348ebfc8ea6b9da55d4e63d0ea3dc3a06ee8
Shaline Davida <shalinedavida@gmail.com>
2025-06-20 16:01:27 +0300

    Created trainings and schedules model

---
+++ coopconnect/trainings/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Trainings(models.Model):
+    topic = models.TextField(max_length=100)
+    description = models.TextField(max_length=100)
+    amount = models.DecimalField(max_digits=10 ,decimal_places=2)
