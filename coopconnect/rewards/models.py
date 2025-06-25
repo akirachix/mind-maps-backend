637313f7c580ebaf2fada4e6a490309d0853c26b
QueenCarine <queencarineh@gmail.com>
2025-06-25 18:58:43 +0300

    Refunds and Attendance apis

--- coopconnect/rewards/models.py
+++ coopconnect/rewards/models.py
@@ -7,5 +7,4 @@ class Rewards(models.Model):
     farmer_id = models.ForeignKey('farmer.Farmer', on_delete=models.CASCADE)
     attendance_id = models.ForeignKey('attendance.Attendance', on_delete=models.CASCADE)
     farmer_points = models.PositiveIntegerField()
-    status = models.CharField(max_length=50)
     last_updated = models.DateTimeField(auto_now=True)
