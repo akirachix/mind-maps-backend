b5f7bb8d2e416eac92aebefa9582d5e697f02e8a
QueenCarine <queencarineh@gmail.com>
2025-06-25 16:37:09 +0300

    Refunds and Attendance apis

--- coopconnect/refunds/views.py
+++ coopconnect/refunds/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from refunds.models import Refund
+from .serializers import RefundSerializer
+
+class RefundViewSet(viewsets.ModelViewSet):
+    queryset=Refund.objects.all()
+    serializer_class=RefundSerializer
