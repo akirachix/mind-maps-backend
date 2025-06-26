d9babebfb7ec6dd3dab0c7e171a20d8541f609be
Shaline Davida <shalinedavida@gmail.com>
2025-06-26 15:37:20 +0300

    Removed files

--- coopconnect/refunds/views.py
+++ coopconnect/refunds/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
-from refunds.models import Refund
-from .serializers import RefundSerializer
-
-class RefundViewSet(viewsets.ModelViewSet):
-    queryset=Refund.objects.all()
-    serializer_class=RefundSerializer
