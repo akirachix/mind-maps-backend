fdd84f0c90f294e2b8d87ce7bcf6d9eac0916a6c
kisanetshshay <shshaykisanet@gmail.com>
2025-07-03 12:35:23 +0300

    Update serializers.py

--- coopconnect/api/serializers.py
+++ coopconnect/api/serializers.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:coopconnect/api/serializers.py
+
 
 
 from rest_framework import serializers
@@ -9,12 +9,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-=======
-from rest_framework import serializers
-from refunds.models import Refund
-
-class RefundSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Refund
-        fields="__all__"
->>>>>>> develop:coopconnect/refunds/serializers.py
