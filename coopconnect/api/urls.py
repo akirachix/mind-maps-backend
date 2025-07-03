d67184d0a765b96e50625091ede72cba4ebc2697
kisanetshshay <shshaykisanet@gmail.com>
2025-07-03 12:35:58 +0300

    Update urls.py

--- coopconnect/api/urls.py
+++ coopconnect/api/urls.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:coopconnect/api/urls.py
+
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -9,15 +9,4 @@ router.register('users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-=======
-from django.urls import path,include
-from rest_framework.routers import DefaultRouter
-from .views import RefundViewSet
-
-router=DefaultRouter()
-router.register(r"",RefundViewSet, basename='refund')
-
-urlpatterns=[
-    path('',include(router.urls)),
->>>>>>> develop:coopconnect/refunds/urls.py
 ]
