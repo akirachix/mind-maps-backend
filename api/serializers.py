8fb1252300030c3da2dfb874254bc951dd123c7f
QueenCarine <queencarineh@gmail.com>
2025-07-24 18:40:31 +0300

    Update AttendanceSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -71,7 +71,7 @@ class AttendanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attendance
         fields = '__all__'
-        read_only_fields = ['id', 'farmer', 'schedule', 'created_at', 'updated_at', 'attended_at']
+        read_only_fields = ['id', 'created_at', 'updated_at', 'attended_at']
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
