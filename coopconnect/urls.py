98741386ed463efbf4f4f08f4a5d19b7a593bf51
Queen Carine <143443488+QueenCarine@users.noreply.github.com>
2025-07-28 17:30:12 +0300

    Update urls.py

--- coopconnect/urls.py
+++ coopconnect/urls.py
@@ -53,6 +53,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("api.urls")),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # <--- FOR LOGIN
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # <--- FOR REFRESHING TOKENS
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
 ]
